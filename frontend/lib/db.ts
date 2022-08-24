708615bc891e393adae3849a9e022897985d6a2b
oicu8 <oicu8@neutroncoin.com>
2022-08-24 17:16:19 -0500

    Delete frontend directory

--- frontend/lib/db.ts
+++
@@ -1,7 +0,0 @@
-// Design not done.
-
-export interface BlockSchema {
-  blockNumber: number;
-  fullySynced: boolean;
-  _id: string;
-}
--- frontend/lib/history.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserHistory, createHashHistory } from "history";
-
-// this will have problem on GitHub pages because of the basename.
-// export default createBrowserHistory();
-
-export default createHashHistory();
